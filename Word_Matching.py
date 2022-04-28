e926f2cc97429a795ad2c122f62af42ed60d4b61
Harikrishna P S <harikrishna.p@zerone-consulting.com>
2022-04-28 22:18:21 +0530

    Project File Added

---
+++ Word_Matching.py
@@ -0,0 +1,14 @@
+from turtle import color
+import streamlit as st 
+from fuzzywuzzy import fuzz
+
+
+st.title('Word Matching')
+
+Str1 = st.text_input("Enter the First Word Here...")
+Str2 = st.text_input("Enter the Second Word Here...")
+if st.button('Get Results'):
+    Ratio = fuzz.ratio(Str1.lower(),Str2.lower())
+    st.write("The Matching Ratio is ")
+    st.subheader(Ratio)
+
